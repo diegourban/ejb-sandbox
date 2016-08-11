52a75b8f02af833233560a9073f0c0f4c39c4e5b
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-11 19:37:26 -0300

    Integrando com JPA

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	@Inject
-	private Banco banco;
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@PostConstruct
 	void aposCriacao() {
@@ -30,16 +31,16 @@ public class AutorDao {
 		// e.printStackTrace();
 		// }
 
-		banco.save(autor);
+		entityManager.persist(autor);
 		System.out.println("[INFO] Salvou o Autor " + autor.getNome());
 	}
 
 	public List<Autor> todosAutores() {
-		return banco.listaAutores();
+		return entityManager.createQuery("select a from Autor a", Autor.class).getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
-		Autor autor = this.banco.buscaPelaId(autorId);
+		Autor autor = this.entityManager.find(Autor.class, autorId);
 		return autor;
 	}
 
