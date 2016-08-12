b2f74490b054f65692cb905f38e2182aa0025ac3
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-12 18:24:15 -0300

    Testando transações

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,25 +4,33 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
+// @TransactionManagement(TransactionManagementType.BEAN) // por padrão é
+// CONTAINER, caso queira que o bean gerencie a transação, use BEAN
 public class AutorDao {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+
+	@Inject
+	UserTransaction tx; // para gerenciar programaticamente pelo bean
 
 	@PostConstruct
 	void aposCriacao() {
 		System.out.println("[INFO] AutorDao foi criado.");
 	}
 
+	@TransactionAttribute(TransactionAttributeType.MANDATORY)
 	public void salva(Autor autor) {
-		System.out.println("[INFO] Salvando o Autor " + autor.getNome());
-
 		// 20 segundos para simular a criação de outro ejb quando esse estriver
 		// sendo usado
 		// try {
@@ -31,8 +39,10 @@ public class AutorDao {
 		// e.printStackTrace();
 		// }
 
+		
+		// tx.begin(); // quando o transaction manager é o bean
 		entityManager.persist(autor);
-		System.out.println("[INFO] Salvou o Autor " + autor.getNome());
+		// tx.commit(); // quando o transaction manager é o bean
 	}
 
 	public List<Autor> todosAutores() {
