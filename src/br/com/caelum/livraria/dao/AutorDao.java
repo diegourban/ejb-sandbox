a45544f3e9b02f0544fbd1523b8647763c6dbaa6
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-13 14:04:30 -0300

    interceptors

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -6,15 +6,18 @@ import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.exception.LivrariaException;
+import br.com.caelum.livraria.interceptador.LogInterceptador;
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 // @TransactionManagement(TransactionManagementType.BEAN) // por padrão é
 // CONTAINER, caso queira que o bean gerencie a transação, use BEAN
+@Interceptors({LogInterceptador.class})
 public class AutorDao {
 
 	@PersistenceContext
---
+++ src/br/com/caelum/livraria/interceptador/LogInterceptador.java
@@ -0,0 +1,24 @@
+package br.com.caelum.livraria.interceptador;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+public class LogInterceptador {
+
+	@AroundInvoke
+	public Object intercepta(InvocationContext context) throws Exception {
+		long millis = System.currentTimeMillis();
+
+		// chamada do método do dao
+		Object o = context.proceed();
+
+		String nomeClasse = context.getTarget().getClass().getSimpleName();
+        String nomeMetodo = context.getMethod().getName();
+        System.out.println("[INFO] " + nomeClasse + "->" + nomeMetodo);
+		System.out.println("[INFO] Tempo gasto no acesso ao BD: "
+                + (System.currentTimeMillis() - millis) + "ms");
+
+		return o;
+	}
+
+}
